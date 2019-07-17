79a0612de161b37b16b5c328d1e8e2373953c2db
75912001 <75912001@qq.com>
2019-07-17 14:30:42 +0800

    日志

---
+++ include/xr_log.h
@@ -0,0 +1,182 @@
+//用于记录日志，一共分9种日志等级。
+//注意,每条日志不能超过8000字节。
+//init
+//update_time...
+
+//旧版本
+//只在linux生效 使用线程
+// 目前使用线程的话效率比不使用还低,应该在于NEW 和 delete ,加锁的过程.
+// 不使用线程 使用 99秒 打印10004990行日志 101060行/秒(14.49W行/秒, 4G内存)
+// 使用线程  使用 151秒 打印10004990行日志  66258行/秒
+
+//当前版本
+//  centos7 3.10.0-957.el7.x86_64
+//  处理器名称：	Intel Core i7
+//  处理器速度：	2.2 GHz
+//  处理器数目：	4
+//  内存：	1 GB
+//  硬盘: 固态
+
+//gcc -O2
+//直接write写入 
+//1000w写(478888900 = 478.88MB)14696504微妙=14.69秒 速度:68.4W行/s
+//线程 写入(写入缓冲中的时间)缓冲大小4194304
+//1000w写(478888900 = 478.88MB)5005706微妙=5.01秒 速度:199.6W行/s
+//线程+semaphore
+//1000w写(478888900 = 478.88MB)5107390微妙=5.11秒 速度:195.7W行/s
+
+#pragma once
+
+//使用线程记录日志
+#define XR_USE_LOG_THREAD
+
+#ifdef XR_USE_LOG_THREAD
+	#include "xr_ring_buf.h"
+	#include "xr_semaphore.h"
+	#include "xr_thread.h"
+#endif
+
+namespace xr{
+	class log_thread_t
+#ifdef XR_USE_LOG_THREAD
+	: public thread_t{
+	public:
+		ring_buf_t ring_buf;
+		semaphore_t semaphore;
+		virtual void do_work_fn(void* data);
+#else
+	{
+#endif
+	};
+
+	//日志等级
+	enum E_LOG_LEVEL {
+		//system is unusable
+		E_LOG_LEVEL_EMERG = 0,
+		//action must be taken immediately
+		E_LOG_LEVEL_ALERT = 1,//黄色
+		//critical conditions
+		E_LOG_LEVEL_CRITI = 2,//深蓝
+		//error conditions
+		E_LOG_LEVEL_ERROR = 3,
+		//warning conditions
+		E_LOG_LEVEL_WARNI = 4,
+		//normal but significant condition
+		E_LOG_LEVEL_NOTIC = 5,
+		//informational
+		E_LOG_LEVEL_INFOR = 6,
+		E_LOG_LEVEL_DEBUG = 7,
+		E_LOG_LEVEL_TRACE = 8,
+		E_LOG_LEVEL_MAX = 9,
+	};
+
+	//日志输出位置(屏幕/文件)
+	enum E_LOG_DEST {//使用BIT位来写入数据.
+		//仅输出到屏幕
+		E_LOG_DEST_TERMINAL	= 1,
+		//仅输出到文件
+		E_LOG_DEST_FILE		= 2,
+		//既输出到屏幕，也输出到文件
+		E_LOG_DEST_BOTH		= 3
+	};
+	//日志的文件描述符
+	struct log_fds_t {
+		int		seq;//顺序号
+		int		fd;//文件FD
+		int		day;//一年中的天数20190626
+		log_fds_t();
+		virtual ~log_fds_t();
+		void init();
+	};
+
+	class log_t{
+	public:
+		log_fds_t fds;
+		log_thread_t thread;
+	public:
+		log_t();
+		//初始化日志记录功能.按时间周期创建新的日志文件
+		//dir:日志保存目录.如果填NULL,则在屏幕中输出日志
+		//lvl:日志输出等级.如果设置为E_LOG_LEVEL_DEBUG,则E_LOG_LEVEL_DEBUG以上等级的日志都不输出
+		//pre_name:日志文件名前缀
+		//logtime:每个日志文件保存logtime分钟的日志,最大不能超过1440分钟
+		//假设logtime为15,则每小时产生4个日志文件,每个文件保存15分钟日志
+		//return:成功返回0,失败返回-1
+		int init(const char* dir, xr::E_LOG_LEVEL lvl, const char* pre_name, uint32_t logtime);
+		//由外部更新的时间(避免内部重复调用系统函数获取时间)
+		inline void update_time(time_t log_sec, struct tm log_tm){
+			this->log_sec = log_sec;
+			this->log_tm = log_tm;
+		}
+
+		#ifdef __GNUC__
+			#define LOG_CHECK_FMT(a,b) __attribute__((format(printf, a, b)))
+		#else
+			#define LOG_CHECK_FMT(a,b)
+		#endif
+
+		void boot(int ok, int space, const char* fmt, ...) LOG_CHECK_FMT(4, 5);
+		void write(int lvl, const char* fmt, ...) LOG_CHECK_FMT(3, 4);
+
+		//切换打开要写入文件的FD
+		//returns:-1:ERROR,>0:返回打开的文件的FD
+		int shift_fd();
+	private:
+		//由外部调用设置的时间(内部使用)
+		time_t log_sec;
+		struct tm log_tm;
+
+		E_LOG_LEVEL level;
+		E_LOG_DEST log_dest;
+		std::string dir_name;//日志目录
+		std::string file_pre_name;//文件名前缀
+		uint32_t logtime_interval_sec;// 每个日志文件记录日志的总时间（秒）
+		/**
+		 * @brief	生成日志文件路径
+		 * @param	char * file_name	产生的日志文件路径
+		 * @param	const struct tm & t_m	时间
+		 */
+		void gen_log_file_path(char* file_name);
+
+		//************************************
+		// Brief:     获取日志序号
+		// Returns:   int	日志序号
+		//************************************
+		int gen_log_seq();
+		int open_fd();
+	};
+	extern log_t* g_log;
+}//end namespace xr
+
+#define DETAIL(level, fmt, args...) \
+	xr::g_log->write(level, "[%s][%d]: " fmt "\n", __FILE__, __LINE__, ##args)
+#define EMERG_LOG(fmt, args...) \
+	DETAIL(xr::E_LOG_LEVEL_EMERG, fmt, ##args)
+#define ALERT_LOG(fmt, args...) \
+	DETAIL(xr::E_LOG_LEVEL_ALERT, fmt, ##args)
+#define CRITI_LOG(fmt, args...) \
+	DETAIL(xr::E_LOG_LEVEL_CRITI, fmt, ##args)
+#define ERROR_LOG(fmt, args...) \
+	DETAIL(xr::E_LOG_LEVEL_ERROR, fmt, ##args)
+#define WARNI_LOG(fmt, args...) \
+	DETAIL(xr::E_LOG_LEVEL_WARNI, fmt, ##args)
+#define NOTIC_LOG(fmt, args...) \
+	DETAIL(xr::E_LOG_LEVEL_NOTIC, fmt, ##args)
+#define INFOR_LOG(fmt, args...) \
+	DETAIL(xr::E_LOG_LEVEL_INFOR, fmt, ##args)
+#define DEBUG_LOG(fmt, args...) \
+	DETAIL(xr::E_LOG_LEVEL_DEBUG, fmt, ##args)
+#define TRACE_LOG(fmt, args...) \
+	DETAIL(xr::E_LOG_LEVEL_TRACE, fmt, ##args)
+
+/**
+ * @def BOOT_LOG
+ * @brief 输出程序启动日志到屏幕,并返回__state__
+ *        用法示例：BOOT_LOG(-1, "dlopen error, %s", error);
+ */
+
+#define BOOT_LOG(__state__, fmt, args...) \
+	do { \
+		xr::g_log->boot(__state__, 0, fmt, ##args); \
+		return __state__; \
+	} while (0)
---
+++ src/xr_log.cpp
@@ -0,0 +1,329 @@
+#include "xr_log.h"
+#include "xr_file.h"
+
+namespace{
+	const uint32_t LOG_BUF_SIZE = 81920; //每条日志的最大字节数
+	const uint32_t RING_BUF_SIZE = 4194304;//环形缓冲大小(2的整数次幂)
+
+	const char* s_log_color[] = { "\e[1m\e[35m", "\e[1m\e[33m", 
+		"\e[1m\e[34m", "\e[1m\e[31m", "\e[1m\e[32m", "\e[1m\e[36m",
+		"\e[1m\e[1m", "\e[1m\e[37m", "\e[1m\e[37m"};
+	const char* s_color_end = "\e[m";
+
+	static const char* log_names[] = { "emerg", "alert", "criti", "error", "warni", "notic", "infor", "debug", "trace" };
+}//end of namespace 
+
+namespace xr{
+log_t* g_log = NULL;
+int log_t::init( const char* dir, E_LOG_LEVEL lvl, const char* pre_name, uint32_t logtime )
+{
+	if(1440 < logtime){
+		::fprintf(stderr, "init log logtime too big=%u!!!\n", logtime);
+		BOOT_LOG(FAIL, "log [dir:%s, per_name:%s, interval_time:%u]", dir, pre_name, logtime);
+	}
+
+	if (NULL == dir || (0 == ::strlen(dir))) {
+		::fprintf(stderr, "init log dir is NULL!!!\n");
+		BOOT_LOG(SUCC, "log [dir:%s, per_name:%s, interval_time:%u]", dir, pre_name, logtime);
+	}
+
+	if ((lvl < E_LOG_LEVEL_EMERG) || (lvl >= E_LOG_LEVEL_MAX)) {
+		::fprintf(stderr, "init log error, invalid log level=%d\n", lvl);
+		BOOT_LOG(FAIL, "log [dir:%s, per_name:%s, interval_time:%u]", dir, pre_name, logtime);
+	}
+
+	//必须可写
+	if (0 != ::access(dir, W_OK)) {
+		::fprintf(stderr, "access log dir %s error\n", dir);
+		BOOT_LOG(FAIL, "log [dir:%s, per_name:%s, interval_time:%u]", dir, pre_name, logtime);
+	}
+#ifdef XR_USE_LOG_THREAD
+	if (SUCC != this->thread.semaphore.init()){
+		BOOT_LOG(FAIL, "log [dir:%s, per_name:%s, interval_time:%u]", dir, pre_name, logtime);
+	}
+	if (SUCC != this->thread.ring_buf.create(RING_BUF_SIZE)){
+		BOOT_LOG(FAIL, "log [dir:%s, per_name:%s, interval_time:%u]", dir, pre_name, logtime);
+	}
+	if (SUCC != this->thread.start(this)){
+		BOOT_LOG(FAIL, "log [dir:%s, per_name:%s, interval_time:%u]", dir, pre_name, logtime);
+	}
+#endif
+	this->dir_name = dir;
+	this->level = lvl;
+	if (NULL == pre_name){
+		this->file_pre_name.clear();
+	} else {
+		this->file_pre_name = pre_name;
+	}
+	this->logtime_interval_sec = logtime * 60;
+
+	this->fds.init();
+	this->fds.seq  = gen_log_seq();
+
+	this->log_dest = E_LOG_DEST_FILE;
+	return SUCC;
+}
+
+void log_t::boot( int ok, int space, const char* fmt, ... )
+{
+	int end, pos;
+	va_list ap;
+
+	static char log_buffer[LOG_BUF_SIZE];
+
+	va_start(ap, fmt);
+	end = ::vsprintf(log_buffer, fmt, ap);
+	va_end(ap);
+
+	static const int SCREEN_COLS = 80;
+	pos = SCREEN_COLS - 10 - (end - space) % SCREEN_COLS;
+	int i = 0;
+	for (; i < pos; i++){
+		log_buffer[end + i] = ' ';
+	}
+	log_buffer[end + i] = '\0';
+
+	static const char* BOOT_OK = "\e[1m\e[32m[ OK ]\e[m";
+	static const char* BOOT_FAIL = "\e[1m\e[31m[ ERR ]\e[m";
+
+	::strcat(log_buffer, SUCC == ok ? BOOT_OK : BOOT_FAIL);
+	::printf("\r%s\n", log_buffer);
+
+	if (0 != ok){
+		::exit(ok);
+	}
+}
+
+int log_t::shift_fd()
+{
+	if ( unlikely((this->fds.fd < 0) && (this->open_fd() < 0)) ) {
+		return ERR;
+	}
+
+	if (0 != this->logtime_interval_sec) {
+		if (likely(this->fds.seq == gen_log_seq())) {
+			return SUCC;
+		}
+	} else {
+		if (likely((this->fds.day == this->log_tm.tm_yday))) {
+			return SUCC;
+		}
+	}
+
+	file_t::close_fd(this->fds.fd);
+
+	if (0 != this->logtime_interval_sec) {
+		this->fds.seq = gen_log_seq();
+	}
+
+	int ret = open_fd();
+
+	return ret;
+}
+
+int log_t::gen_log_seq()
+{
+	return (int)(0 == this->logtime_interval_sec ? 0 : (uint32_t)this->log_sec / this->logtime_interval_sec);
+}
+
+int log_t::open_fd()
+{
+	//O_APPEND 有该选项,write时是线程安全的.请看本页write函数
+	int flag = O_WRONLY | O_CREAT | O_APPEND/* | O_LARGEFILE*/;
+
+	char file_name[FILENAME_MAX];
+	gen_log_file_path(file_name);
+
+	this->fds.fd = ::open(file_name, flag, 0644);
+	if (INVALID_FD != this->fds.fd) {
+		if (this->fds.day != this->log_tm.tm_yday ) {
+			this->fds.day = this->log_tm.tm_yday;
+		}
+		file_t::fcntl_add_flag(this->fds.fd, FD_CLOEXEC);
+	}
+
+	return this->fds.fd;
+}
+
+void log_t::gen_log_file_path( char* file_name )
+{
+	if (0 != this->logtime_interval_sec) {
+		time_t t = this->log_sec / this->logtime_interval_sec * this->logtime_interval_sec;
+		struct tm tmp_tm;
+		::localtime_r(&t, &tmp_tm);
+
+		::sprintf(file_name, "%s/%s%04d%02d%02d%02d%02d.txt", 
+			this->dir_name.c_str(), this->file_pre_name.c_str(),
+			tmp_tm.tm_year + 1900, tmp_tm.tm_mon + 1, 
+			tmp_tm.tm_mday, tmp_tm.tm_hour, tmp_tm.tm_min);
+	} else {
+		::sprintf(file_name, "%s/%s%04d%02d%02d.txt", 
+			this->dir_name.c_str(), this->file_pre_name.c_str(),
+			this->log_tm.tm_year + 1900, this->log_tm.tm_mon + 1, this->log_tm.tm_mday);
+	}
+}
+
+log_t::log_t()
+{
+	this->level = E_LOG_LEVEL_INFOR;
+	this->log_dest = E_LOG_DEST_TERMINAL;
+	this->logtime_interval_sec = 0;
+	{		
+		this->log_sec = ::time(NULL);
+		::localtime_r(&this->log_sec, &this->log_tm);
+	}
+}
+
+#ifdef XR_USE_LOG_THREAD
+void log_t::write( int lvl, const char* fmt, ... )
+{
+	if (lvl > this->level) {
+		return;
+	}
+
+	va_list ap;
+
+	va_start(ap, fmt);
+
+	if (unlikely(E_LOG_DEST_TERMINAL & this->log_dest)) {
+		va_list aq;
+		va_copy(aq, ap);
+		switch (lvl) {
+		case E_LOG_LEVEL_EMERG:
+		case E_LOG_LEVEL_ALERT:
+		case E_LOG_LEVEL_CRITI:
+		case E_LOG_LEVEL_ERROR:
+			::fprintf(stderr, "%s%02d:%02d:%02d ", s_log_color[lvl],
+				this->log_tm.tm_hour, this->log_tm.tm_min, this->log_tm.tm_sec);
+			::vfprintf(stderr, fmt, aq);
+			::fprintf(stderr, "%s", s_color_end);
+			break;
+		default:
+			::fprintf(stdout, "%s%02d:%02d:%02d ", s_log_color[lvl],
+				this->log_tm.tm_hour, this->log_tm.tm_min, this->log_tm.tm_sec);
+			::vfprintf(stdout, fmt, aq);
+			::fprintf(stdout, "%s", s_color_end);
+			break;
+		}
+		va_end(aq);
+	}
+
+	if (unlikely(!(this->log_dest & E_LOG_DEST_FILE))) {
+		va_end(ap);
+		return;
+	}
+
+	static char log_buffer[LOG_BUF_SIZE];
+
+	int pos = snprintf(log_buffer, sizeof(log_buffer), "[%02d:%02d:%02d][%s]",
+		this->log_tm.tm_hour, this->log_tm.tm_min, this->log_tm.tm_sec, log_names[lvl]);
+
+	int end = ::vsnprintf(log_buffer + pos, sizeof(log_buffer) - pos, fmt, ap);
+	va_end(ap);
+
+	uint32_t len = end+pos;
+	uint32_t write_len = 0;
+	do{
+		uint32_t n = this->thread.ring_buf.write(log_buffer + write_len, len - write_len);
+		#if RING_BUF_SIZE < LOG_BUF_SIZE
+		if (0 < n){
+			//RING_BUF_SIZE < LOG_BUF_SIZE 时 信号量放在此处发送
+			this->thread.semaphore.post();
+		}
+		#endif
+		write_len += n; 
+	}while(len != write_len);
+	#if LOG_BUF_SIZE <= RING_BUF_SIZE
+		//LOG_BUF_SIZE <= RING_BUF_SIZE 时 信号量放在此处发送
+		this->thread.semaphore.post();
+	#endif
+}
+#else
+void log_t::write( int lvl, const char* fmt, ... )
+{
+	if (lvl > this->level) {
+		return;
+	}
+
+	va_list ap;
+
+	va_start(ap, fmt);
+
+	if (unlikely(E_LOG_DEST_TERMINAL & this->log_dest)) {
+		va_list aq;
+		va_copy(aq, ap);
+		switch (lvl) {
+		case E_LOG_LEVEL_EMERG:
+		case E_LOG_LEVEL_ALERT:
+		case E_LOG_LEVEL_CRITI:
+		case E_LOG_LEVEL_ERROR:
+			::fprintf(stderr, "%s%02d:%02d:%02d ", s_log_color[lvl],
+				this->log_tm.tm_hour, this->log_tm.tm_min, this->log_tm.tm_sec);
+			::vfprintf(stderr, fmt, aq);
+			::fprintf(stderr, "%s", s_color_end);
+			break;
+		default:
+			::fprintf(stdout, "%s%02d:%02d:%02d ", s_log_color[lvl],
+				this->log_tm.tm_hour, this->log_tm.tm_min, this->log_tm.tm_sec);
+			::vfprintf(stdout, fmt, aq);
+			::fprintf(stdout, "%s", s_color_end);
+			break;
+		}
+		va_end(aq);
+	}
+
+	if (unlikely(!(this->log_dest & E_LOG_DEST_FILE) || (shift_fd() < 0))) {
+		va_end(ap);
+		return;
+	}
+
+	char log_buffer[LOG_BUF_SIZE];
+
+	int pos = snprintf(log_buffer, sizeof(log_buffer), "[%02d:%02d:%02d][%s]",
+		this->log_tm.tm_hour, this->log_tm.tm_min, this->log_tm.tm_sec, log_names[lvl]);
+
+	int end = ::vsnprintf(log_buffer + pos, sizeof(log_buffer) - pos, fmt, ap);
+	va_end(ap);
+
+	//判断返回值(会被信号中断等处理方式).. on linux :  man write 
+	//多线程写文件,这里不需要上锁!请 man 2 write 
+	// If the file  was open(2)ed  with  O_APPEND,  
+	// the file offset is first set to the end of the file before writing.
+	// The adjustment of the file offset and the write operation are performed as an atomic step.
+	HANDLE_EINTR(::write(this->fds.fd, log_buffer, end + pos));
+}
+#endif
+
+void log_fds_t::init()
+{
+	this->seq = 0;
+	this->fd = INVALID_FD;
+	this->day = 0;
+}
+
+log_fds_t::log_fds_t()
+{
+	this->init();
+}
+
+log_fds_t::~log_fds_t()
+{
+	file_t::close_fd(this->fd);
+}
+
+#ifdef XR_USE_LOG_THREAD
+void log_thread_t::do_work_fn( void* data )
+{
+	log_t* log = (log_t*)data;
+	log->thread.semaphore.wait();
+	if (unlikely(log->shift_fd() < 0)) {
+		return;
+	}
+
+	static char log_buffer[RING_BUF_SIZE];
+	uint32_t n = this->ring_buf.read(log_buffer, sizeof(log_buffer));
+
+	HANDLE_EINTR(::write(log->fds.fd, log_buffer, n));
+}
+#endif
+}//end namespace xr
