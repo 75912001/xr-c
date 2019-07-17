89e9b71f2f40643f84286d6ba913bfa4ee9c2796
75912001 <75912001@qq.com>
2019-07-17 11:55:12 +0800

    处理以tab为分隔符的文件

---
+++ include/xr_file_tab.h
@@ -0,0 +1,56 @@
+//以tab为分隔符的文件行
+//id	name	desc	icon	
+//物品id	名字	描述	iconid资源id	
+//1	物品1	描述1	7,3,1	
+//2	物品2	描述2	7,3,2	
+//3	物品3	描述3	7,3,3	
+//4	物品4	描述4	7,3,4	
+//5	物品5	描述5	7,3,5	
+//6	物品6	描述6	7,3,6	
+
+#pragma once
+
+#include "xr_util.h"
+
+namespace xr{
+	struct file_tab_t{
+	public:
+		//加载文件到内存
+		int load(const char* file_path);
+		//传入 id,name,icon
+		//返回 多行,  每行为id,name,icon组成的vector
+		std::vector<std::vector<std::string>> get(const std::vector<std::string>& title_vec);
+	private:
+		std::map<std::string, uint32_t> title_map;//key:符串, val:字col列[0-n]
+		std::vector<std::vector<std::string>> content_vector;//内容 字符串信息 
+
+		template <typename T>
+		inline T get_val_def(const char* col_name, const std::vector<std::string>& line, T def){
+			T ret;
+			return SUCC == this->get_val(col_name, line, ret) ? ret : def;
+		}
+		//获取指定的值
+		//returns:0:成功.其它:失败
+		//data(传出值)
+		template <typename T>
+		inline int get_val(const char* col_name, const std::vector<std::string>& line, T& data){
+			uint32_t col = 0;//要取得值的列数
+			{
+				auto it = this->title_map.find(col_name);
+				if (this->title_map.end() == it){
+					return FAIL;
+				}
+				col = it->second;
+			}
+			{
+				std::string val = line[col];
+				if (val.empty()){
+					return FAIL;
+				}
+							
+				string2T(val, data);
+			}
+			return SUCC;
+		}
+	};
+}//end namespace xr
---
+++ src/xr_file_tab.cpp
@@ -0,0 +1,77 @@
+#include "xr_file_tab.h"
+
+namespace {
+	//解析文件
+int __parser( const std::vector<std::string>& line_vector, 
+	std::map<std::string, uint32_t>& title_map,
+	std::vector<std::vector<std::string>>& content_vector ){
+	std::string cur_section_name;//当前章节名称
+	uint32_t row = 0;//行
+	FOREACH(line_vector, it){
+		const std::string& line = *it;
+		row++;
+ 		if (2 == row){
+ 			continue;
+ 		}
+		std::vector<std::string> val_vector;
+		cat_string(line, '	', val_vector);
+		if (1 == row){
+			uint32_t col_cnt = 0;//列
+			FOREACH(val_vector, it_title){
+				title_map[*it_title] = col_cnt++;
+			}
+			continue;
+		}
+		if (title_map.size() != val_vector.size()){//值的列数和标题的列数不等
+			return FAIL;
+		}
+		content_vector.push_back(val_vector);
+	}
+	return SUCC;
+}
+}
+namespace xr{
+int file_tab_t::load( const char* file_path )
+{
+	std::ifstream fin(file_path);
+	if (!fin.good()){
+		fin.close();
+		return FAIL;
+	}
+
+	std::vector<std::string> line_vector;
+	std::string line;
+
+	while(std::getline(fin, line)){
+		if (line.empty()){//去除空行
+			continue;
+		}
+		if ('#' == line[0]){//去除注释
+			continue;
+		}
+		line_vector.push_back(line);
+	}
+	if (FAIL == __parser(line_vector, this->title_map, this->content_vector)){
+		fin.close();
+		return FAIL;
+	}
+	fin.close();
+	return SUCC;
+}
+
+std::vector<std::vector<std::string>> file_tab_t::get(const std::vector<std::string>& title_vec){
+	std::vector<std::vector<std::string>> ret;
+	FOREACH(this->content_vector, it){
+		std::vector<std::string>& line = *it;
+		std::vector<std::string> str_vac;
+		FOREACH(title_vec, it_title){
+			const std::string& col_name = *it_title;
+			static std::string str_def;
+			std::string val = this->get_val_def(col_name.c_str(), line, str_def);
+			str_vac.push_back(val);
+		}
+		ret.push_back(str_vac);
+	}
+	return ret;
+}
+}//end namespace xr
