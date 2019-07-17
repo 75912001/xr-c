4ff644146ad06b418caf45d056ba7a520af7b9ee
75912001 <75912001@qq.com>
2019-07-17 11:20:00 +0800

    文件操作

---
+++ include/xr_file.h
@@ -0,0 +1,34 @@
+//文件相关操作
+
+#pragma once
+
+#include "xr_include.h"
+
+namespace xr{
+	struct file_t{
+		//set the given fd to be blocking or noblocking
+		//returns:0:success.-1:error
+		//fd:file descriptor to be set
+		//is_block:true:blocking.false:nonblocking
+		static int set_io_block(int fd, bool is_block);
+		static int fcntl_add_flag(int fd, int add_flag);
+
+		//close the given fd(if fd is VALID),and set to be -1 
+		//returns:0:success.-1:error
+		//其中判定 INVALID_FD != fd  时才做关闭操作
+		static int close_fd(int& fd);
+
+		//Disables further receive operations.关闭socket可读
+		//returns:0:success.-1:error
+		//其中判定 INVALID_FD != fd  时才做关闭操作
+		//************************************
+		static int shutdown_rd(int fd);
+
+		//得到目录下的文件
+		//returns:0:OK.其它:失败
+		//path 目录的路径
+		// file_names  返回的文件名
+		static int get_dir_file(const char* path, std::vector<std::string>& file_names);
+	};
+
+}//end namespace xr
---
+++ src/xr_file.cpp
@@ -0,0 +1,65 @@
+#include "xr_file.h"
+#include "xr_util.h"
+
+namespace xr{
+int file_t::get_dir_file( const char *path, std::vector<std::string>& file_names )
+{
+	DIR* dirp;
+	dirp = ::opendir(path);
+	if (NULL != dirp) {
+		while(1) {
+			struct dirent* direntp;
+			direntp = ::readdir(dirp);
+			if ( NULL == direntp ) {
+				break;
+			}
+			if (DT_REG == direntp->d_type) {
+				file_names.push_back(direntp->d_name);
+			}
+		}
+		::closedir(dirp);
+	} else {
+		return FAIL;
+	}
+	return SUCC;
+}
+
+int file_t::set_io_block( int fd, bool is_block )
+{
+	int val;
+	if (is_block) {
+		val = (~O_NONBLOCK & ::fcntl(fd, F_GETFL));
+	} else {
+		val = (O_NONBLOCK | ::fcntl(fd, F_GETFL));
+	}
+	return ::fcntl(fd, F_SETFL, val);
+}
+
+int file_t::fcntl_add_flag( int fd, int add_flag )
+{
+	int flag  = ::fcntl(fd, F_GETFD, 0);
+	flag |= add_flag;
+	return ::fcntl(fd, F_SETFD, flag);
+}
+
+int file_t::close_fd( int& fd )
+{
+	int ret = SUCC;
+	if (INVALID_FD != fd){
+		ret = HANDLE_EINTR(::close(fd));
+		if(ERR != ret){
+			fd = INVALID_FD;
+		}
+	}
+	return ret;
+}
+
+int file_t::shutdown_rd( int fd )
+{
+	int ret = SUCC;
+	if (INVALID_FD != fd){
+		ret = ::shutdown(fd, SHUT_RD);
+	}
+	return ret;
+}
+}//end namespace xr
